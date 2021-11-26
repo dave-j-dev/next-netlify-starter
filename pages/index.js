ebbd02a151157456d9ae141900641b0f6332ac4d
dave-j-dev <dave.j.dev@gmail.com>
2021-11-25 19:24:21 -0800

    with GH desktop

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to QnCalq Change" />
+        <Header title="Welcome to QnCalq" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
